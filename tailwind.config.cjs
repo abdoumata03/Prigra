9017a6b4d8c5a11eb9fa6a22bd80e5dc09af506b
MATALLAH <a.matallah@esi-sba.dz>
2023-04-02 17:59:15 +0100

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      eudox: ['eudoxus sans', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
